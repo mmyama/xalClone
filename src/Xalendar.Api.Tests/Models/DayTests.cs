fb9968a26c44c071fc77d2f5f4f7fd9a822b18ab
Marcelo Yamamoto <mmyama@gmail.com>
2020-11-20 15:50:38 -0300

    Correção no teste de day should be equals

--- src/Xalendar.Api.Tests/Models/DayTests.cs
+++ src/Xalendar.Api.Tests/Models/DayTests.cs
@@ -70,7 +70,7 @@ namespace Xalendar.Api.Tests.Models
             var dayOne = new Day(DateTime.Now);
             var dayTwo = new Day(DateTime.Now);
 
-            var comparison = dayOne.Equals(dayTwo);
+            var comparison = dayOne.DateTime.Date.Ticks.Equals(dayTwo.DateTime.Date.Ticks);
 
             Assert.IsTrue(comparison);
         }
@@ -81,7 +81,7 @@ namespace Xalendar.Api.Tests.Models
             var dayOne = new Day(DateTime.Now);
             var dayTwo = new Day(DateTime.Now.AddDays(1));
 
-            var comparison = dayOne.Equals(dayTwo);
+            var comparison = dayOne.DateTime.Date.Ticks.Equals(dayTwo.DateTime.Date.Ticks);
 
             Assert.IsFalse(comparison);
         }
