30c6abd70ff216416e7151bfe73071982fab83c9
mmyama@gmail.com <mmyama@gmail.com>
2020-11-25 16:18:01 -0300

    Tentativa de simplificação dos testes

--- src/Xalendar.Api/Models/Month.cs
+++ src/Xalendar.Api/Models/Month.cs
@@ -6,7 +6,7 @@ using static Xalendar.Api.Extensions.MonthExtention;
 
 namespace Xalendar.Api.Models
 {
-    public struct Month : IEquatable<Month>
+    public struct Month
     {
         public DateTime MonthDateTime { get; }
         public IReadOnlyList<Day> Days;
@@ -16,26 +16,6 @@ namespace Xalendar.Api.Models
             MonthDateTime = dateTime;
             Days = GenerateDaysOfMonth(dateTime);
         }
-
-        public bool Equals(Month other)
-        {
-            var yearValue = MonthDateTime.Year == other.MonthDateTime.Year;
-            var monthValue = MonthDateTime.Month == other.MonthDateTime.Month;
-            var days = Days.SequenceEqual(other.Days);
-
-            return yearValue && monthValue;
-            //return yearValue && monthValue && days;
-        }
-
-        public static bool operator ==(Month left, Month right) =>
-            left.Equals(right);
-        public static bool operator !=(Month left, Month right) =>
-            !left.Equals(right);
-
-        public override bool Equals(object obj) =>
-            (obj is Month month) && (this.Equals(month));
-
-        public override int GetHashCode() =>
-            (MonthDateTime, Days).GetHashCode();
     }
+
 }
