125426034a96bd051688e13844f501294fc04565
mmyama@gmail.com <mmyama@gmail.com>
2020-12-02 01:09:27 -0300

    Adição de navigation page no main

--- src/Xalendar.Sample/Xalendar.Sample/App.xaml.cs
+++ src/Xalendar.Sample/Xalendar.Sample/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Xalendar.Sample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
