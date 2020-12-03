4cd580633b04cf058e0a71fb27edb1c2a7e08e35
mmyama@gmail.com <mmyama@gmail.com>
2020-12-03 18:00:09 -0300

    Suporte para descartar espaços referentes aos dias dos meses anterior e posterior

--- src/Xalendar.Api.Tests/Models/MonthContainerTests.cs
+++ src/Xalendar.Api.Tests/Models/MonthContainerTests.cs
@@ -15,14 +15,21 @@ namespace Xalendar.Api.Tests.Models
     public class MonthContainerTests
     {
         [Test]
-        public void MonthContainerShouldContainDaysOfMonth()
+        [TestCaseSource(nameof(MonthsValuesTests))]
+        public void MonthContainerShouldContainDaysOfCurrentNextAndPreviousMonthes(DateTime dateTime, int totalDays)
         {
-            var dateTime = DateTime.Now;
-
             var monthContainer = new MonthContainer(dateTime);
 
             Assert.IsNotEmpty(monthContainer.Days);
+            Assert.AreEqual(totalDays, monthContainer.Days.Count);
         }
+
+        private static object[] MonthsValuesTests =
+        {
+            new object[] {new DateTime(2020, 7, 1), 35},
+            new object[] {new DateTime(2020, 8, 1), 42},
+            new object[] {new DateTime(2015, 2, 1), 28}
+        };
 
         [Test]
         public void MonthContainerCanSelectDay()
--- src/Xalendar.Api/Models/MonthContainer.cs
+++ src/Xalendar.Api/Models/MonthContainer.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Xalendar.Api.Extensions;
 
 namespace Xalendar.Api.Models
@@ -15,15 +16,35 @@ namespace Xalendar.Api.Models
         [EditorBrowsable(EditorBrowsableState.Never)]
         public Month _month;
 
-        public IReadOnlyList<Day> Days { get; }
+        public IReadOnlyList<Day?> Days { get; }
 
         public MonthContainer(DateTime dateTime)
         {
             _month = new Month(dateTime);
-            Days = _month.Days;
+
+            var daysOfContainer = new List<Day?>();
+            GetDayToDiscardAtStartOfMonth(daysOfContainer);
+            daysOfContainer.AddRange(_month.Days);
+            GetDayToDiscardAtEndOfMonth(daysOfContainer);
+            Days = daysOfContainer;
         }
 
+        private void GetDayToDiscardAtStartOfMonth(List<Day?> daysOfContainer)
+        {
+            var firstDay = _month.Days.First();
+            var numberOfDaysToDiscard = (int)firstDay.DateTime.DayOfWeek;
 
+            for (var i = 0; i < numberOfDaysToDiscard; i++)
+                daysOfContainer.Add(default(Day));
+        }
 
+        private void GetDayToDiscardAtEndOfMonth(List<Day?> daysOfContainer)
+        {
+            var lastDay = _month.Days.Last();
+            var numberOfDaysToDiscard = 6 - (int)lastDay.DateTime.DayOfWeek;
+
+            for (var i = 0; i < numberOfDaysToDiscard; i++)
+                daysOfContainer.Add(default(Day));
+        }
     }
 }
