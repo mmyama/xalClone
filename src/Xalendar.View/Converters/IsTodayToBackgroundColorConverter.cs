c18251c47d4a0212983165b92d2ec4716bd96b9c
mmyama@gmail.com <mmyama@gmail.com>
2020-12-07 10:25:19 -0300

    Criação e implementação de converters do IsToday para o BackgroundColor

---
+++ src/Xalendar.View/Converters/IsTodayToBackgroundColorConverter.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+using Xalendar.Api.Models;
+using Xamarin.Forms;
+
+namespace Xalendar.View.Converters
+{
+    public class IsTodayToBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Day day)
+                if (day != null)
+                    return day.IsToday ? Color.Red : Color.Transparent;
+
+            return Color.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
