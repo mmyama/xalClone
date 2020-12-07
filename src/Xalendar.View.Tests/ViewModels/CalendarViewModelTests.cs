c7e34e9c606656e1c634644c3131f1ab822d6f32
mmyama@gmail.com <mmyama@gmail.com>
2020-12-07 10:02:49 -0300

    Criação de ambiente de testes para a ViewModel

---
+++ src/Xalendar.View.Tests/ViewModels/CalendarViewModelTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Xalendar.View.Tests.ViewModels
+{
+    [TestFixture]
+    public class CalendarViewModelTests
+    {
+    }
+}
