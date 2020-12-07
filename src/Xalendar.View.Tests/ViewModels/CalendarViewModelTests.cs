e2ed9306ba32531ed7f5e4defcd9086099354c38
mmyama@gmail.com <mmyama@gmail.com>
2020-12-07 10:06:09 -0300

    Criação de testes para o construtor de CalendarViewModel

--- src/Xalendar.View.Tests/ViewModels/CalendarViewModelTests.cs
+++ src/Xalendar.View.Tests/ViewModels/CalendarViewModelTests.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using Xalendar.View.ViewModels;
 
 namespace Xalendar.View.Tests.ViewModels
 {
     [TestFixture]
     public class CalendarViewModelTests
     {
+        [Test]
+        public void ConstructorShouldPopulateDays()
+        {
+            var viewModel = new CalendarViewModel();
+
+            Assert.NotNull(viewModel.Days);
+        }
+
+        [Test]
+        public void ConstructorShouldPopulateDaysOfWeek()
+        {
+            var viewModel = new CalendarViewModel();
+
+            Assert.NotNull(viewModel.DaysOfWeek);
+        }
+
     }
 }
