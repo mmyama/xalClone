d126642716199b68ea8aff5e9b218074edfabc2d
mmyama@gmail.com <mmyama@gmail.com>
2020-12-08 18:45:30 -0300

    Criação do suporte para visualização de eventos e seus testes

---
+++ src/Xalendar.View.Tests/Converters/HasEventsToVisibilityConverterTests.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using NUnit.Framework;
+using Xalendar.Api.Extensions;
+using Xalendar.Api.Models;
+using Xalendar.View.Converters;
+
+namespace Xalendar.View.Tests.Converters
+{
+    [TestFixture]
+    public class HasEventsToVisibilityConverterTests
+    {
+        [Test]
+        public void VisibilityShouldBeFalseIfDayIsNull()
+        {
+            var day = default(Day);
+            var converter = new HasEventsToVisibilityConverter();
+
+            var result = (bool)converter.Convert(day, null, null, null);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void VisibilityShouldBeFalseIfDayDoesntHaveEvents()
+        {
+            var day = new Day(DateTime.Now);
+            var converter = new HasEventsToVisibilityConverter();
+
+            var result = (bool)converter.Convert(day, null, null, null);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void VisibilityShouldBeTrueIfDayHasEvents()
+        {
+            var dateTime = DateTime.Now;
+            var day = new Day(dateTime);
+            day.AddEvent(new Event(1, "Name", dateTime, dateTime, false));
+            var converter = new HasEventsToVisibilityConverter();
+
+            var result = (bool)converter.Convert(day, null, null, null);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
--- src/Xalendar.View/Converters/HasEventsToVisibilityConverter.cs
+++ src/Xalendar.View/Converters/HasEventsToVisibilityConverter.cs
@@ -3,12 +3,25 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
+using Xalendar.Api.Models;
+using Xamarin.Forms;
 
 namespace Xalendar.View.Converters
 {
-    public class HasEventsToVisibilityConverter
+    public class HasEventsToVisibilityConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Day day)
+                return day.HasEvents;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
